71a3a8779a78073683085a4469638e02fd20660a
kwkim91919 <83797430+kwkim91919@users.noreply.github.com>
2021-11-08 15:37:58 +0900

    Add files via upload

---
+++ main.js
@@ -0,0 +1,16 @@
+$(function(){
+    $('#fullpage').fullpage({
+		//options here
+        anchors:['section1', 'section2', 'section3','section4'],
+        navigation:true,
+        navigationPosition:'right',
+        sectionsColor : ['#2d90ff','#e3606b','#23b6a6','#f6c444']
+	
+    
+    });
+
+    
+});
+
+
+
